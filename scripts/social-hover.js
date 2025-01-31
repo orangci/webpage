01ec96e4a7aa923ae9a59cdb21c6ddb6585eac08
orangci <c@orangc.net>
2025-01-31 15:33:09 +0300

    add cirno pfp

--- scripts/social-hover.js
+++ scripts/social-hover.js
@@ -1,17 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const socialLinks = document.querySelectorAll('.social-link');
   const usernameElement = document.getElementById('username');
   const originalUsername = usernameElement.innerHTML;
 
   socialLinks.forEach(link => {
-    link.addEventListener('mouseover', function() {
+    link.addEventListener('mouseover', function () {
       const newUsername = this.getAttribute('data-username');
-      if (newUsername) {
-        usernameElement.innerHTML = `<img src="https://avatars.githubusercontent.com/u/90866414?size=48" alt="pfp" class="self-center pl-0 mx-2 mb-0 ml-0 size-12">${newUsername}`;
+      const cirnoUsernames = ['discord', 'goodreads', 'myanimelist'];
+      if (cirnoUsernames.includes(newUsername)) {
+        usernameElement.innerHTML = `<img src="./assets/cirno.png" alt="pfp" class="self-center pl-0 mx-2 mb-0 ml-0 rounded-full size-12">orangc`;
+      } else if (newUsername) {
+        usernameElement.innerHTML = `<img src="./assets/leaf-small.png" alt="pfp" class="self-center pl-0 mx-2 mb-0 ml-0 size-12">${newUsername}`;
       }
     });
 
-    link.addEventListener('mouseout', function() {
+    link.addEventListener('mouseout', function () {
       usernameElement.innerHTML = originalUsername;
     });
   });
